de83ba380a017e320544e50ac97b5fd7186cc2a4
Neha <nehasijaria@gmail.com>
2022-01-18 10:08:52 -0500

    access allTodos props

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Todo() {
+export default function Todo(props) {
   
   return (
     <div className='container'>
       <h3>Todos List</h3>       
-      
+      {props.myTodos}
     </div>
   )
 }
