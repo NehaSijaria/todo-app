33032e7fb889fc782476e60dc472973dbf242755
Neha <nehasijaria@gmail.com>
2022-01-18 12:52:04 -0500

    access props from child

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,10 +2,10 @@ import React from 'react'
 // import TodoItem from './TodoItem'
 
 export default function Todo(props) {
-  
+  console.log('props--', props.myTodos)
   return (
     <div className='container'>
-      <h3>Todos List</h3>       
+      <h3>Todos List</h3>  
       {/* {props.myTodos} */}
       {/* <TodoItem singleTodo={props.myTodos}/> */}
     </div>
