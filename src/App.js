549346b519cdadc3dff014624632d5feff7b8d0f
Neha <nehasijaria@gmail.com>
2022-01-17 19:01:11 -0500

    cleanup file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div>
-      <Navbar title="MyTodoApp" status="In-progress"/>
+      <Navbar title="MyTodoApp" />
       <TodoItems />
       <Footer />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar(props) {
   // const { title, status } = props;
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">{props.title} {props.status}</a>
+  <a class="navbar-brand" href="#">{props.title}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -15,7 +15,7 @@ export default function Navbar(props) {
         <a class="nav-link" href="#">Home </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">About</a>
       </li>
       
     </ul>
