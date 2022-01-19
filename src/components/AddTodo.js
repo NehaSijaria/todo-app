6a30daaaf5f48ee12ac1730ca5ad86649c4d77fe
Neha <nehasijaria@gmail.com>
2022-01-19 10:40:20 -0500

    modify form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React from "react";
 
 export default function AddTodo() {
   return (
-    <div>
+    <div className="container my-3">
+      <h3> Add Todo</h3>
       <form>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
-  </div>
-  <div className="form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" className="btn btn-primary">Submit</button>
-</form>
+        <div className="mb-3">
+          <label for="title">Todo Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter Todo Title"
+          />
+          
+        </div>
+        <div className="mb-3">
+          <label for="desc">Todo Description</label>
+          <input
+            type="text"
+            className="form-control"
+            id="desc"
+            placeholder="description"
+          />
+        </div>
+        
+        <button type="submit" className="btn btn-sm btn-success">
+          Add Todo
+        </button>
+      </form>
     </div>
-  )
+  );
 }
